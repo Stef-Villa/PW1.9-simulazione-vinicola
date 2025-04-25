a28e90ec0634494bc0863ac0297c16951c0c23cc
Stef-Villa <stefavill@gmail.com>
2025-04-26 00:43:55 +0200

    Update addestrametno_produzione.py

--- app_generazione/archive/addestrametno_produzione.py
+++ app_generazione/archive/addestrametno_produzione.py
@@ -4,38 +4,36 @@ import numpy as np
 import pandas as pd
 import joblib 
 
-# Caricare il file CSV per visualizzare i dati
+# file CSV con dati sotrici
 file_path = r'data\csv_dati_reali_produzione_meteo_piemonte.csv'
 df = pd.read_csv(file_path, sep=';')
 
-# Mostrare le prime righe per verificare la corretta lettura del file
 print(df.head())
 
-# Calcolare la produzione per ettaro
+# Calcolo produzione per ettaro
 df['produzione_per_ettaro'] = df['produzione_uva_quintali'] / df['superficie_ettari']
 
-# Funzione per eseguire la Random Forest sulla produzione per ettaro
+# Random Forest sulla produzione per ettaro
 def random_forest_produzione_ettaro(df):
-    # Selezioniamo le variabili indipendenti (features) e la variabile dipendente (target)
+    # ariabili indipendenti e la variabile dipendente 
     X = df[['temp_media', 'umidita_media', 'pioggia_mm', 'vento_kmh']]
     y = df['produzione_per_ettaro']
     
-    # Creiamo il modello Random Forest
+    # Creazione modello Random Forest
     model = RandomForestRegressor(n_estimators=100, random_state=42)
     
-    # Addestriamo il modello sui dati
+    # Addestriamento
     model.fit(X, y)
     
-    # Restituiamo il modello addestrato
     return model
 
-# Eseguiamo il modello di Random Forest sui dati
+# Esecuzione modello sui dati
 rf_model = random_forest_produzione_ettaro(df)
 
-# Fare le previsioni con il modello addestrato
+# previsioni con il modello addestrato
 y_pred_rf = rf_model.predict(df[['temp_media', 'umidita_media', 'pioggia_mm', 'vento_kmh']])
 
-# Calcolare R² e RMSE per il modello Random Forest
+# R² e RMSE del modello
 r2_rf = r2_score(df['produzione_per_ettaro'], y_pred_rf)
 rmse_rf = np.sqrt(mean_squared_error(df['produzione_per_ettaro'], y_pred_rf))
 
